e26d202a33190c8d4e9a46c4f45ac30c1d7cd430
Steven Gallagher <s.thomas.gallagher@gmail.com>
2011-06-13 13:55:02 -0500

    Implementing catalog display

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
+  root :to => 'store#index', :as => 'store'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,10 +51,7 @@ Depot::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
   test "product attributes must not be empty" do
    product = Product.new
    assert product.invalid?
@@ -25,7 +27,35 @@ class ProductTest < ActiveSupport::TestCase
 	  product.errors[:price].join9(';')
 
 	product.price = 1
-	assert product.valid?
-				
+	assert product.valid?	
 	end
+	
+  def new_product(image_url)
+    Product.new(:title			=> "My Book Title",
+				:description	=> "yyy",
+				:price			=>1,		  
+				:image_url 		=> "zzz.jpg")
+  end
+  
+  test "image url" do
+    ok  = %w{ fred.gif fred.jpeg fred.png FRED.JPG FRED.Jpg
+			  http://a.b.c/x/y/z/fred.gif }
+	bad = %w{ fred.doc fred.gif/more fred.gif.more }
+	
+	ok.each do |name|
+	  assert new_product(name).valid?, "#{name} shouldn't be invalid"
+	end
+	
+	bad.each do |name|
+	  assert new_product(name).invalid?, "#{name} shouldn't be valid"
+	end
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(:title        => products(:ruby).title,
+						  :description	=> "yyy",
+						  :price		=> 1,		  
+						  :image_url 	=> "zzz.jpg")
+	assert !product.save
+	assert_equal "has already been taken", product.errors[:title].join(';')
+  end
 end
